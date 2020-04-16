4a4786fb6bfcd3f0a3141342630842bcde2c452c
Rodolfo Oliveira Lorenzo <rodolfo@ibureau.com.br>
2020-04-16 12:05:46 -0300

    function to convert OD bases to sf

--- 901_convert_spatial.R
+++ 901_convert_spatial.R
@@ -1,13 +1,13 @@
+#funcao para converter as bases da OD para sf
 
-
-convert_spatial <- function(database, projection, origem){
+convert_spatial <- function(database, projection, origem = T){
   require(sf)
-  require(dpyr)
+  require(dplyr)
   
   if(origem == T){coord_name <- c("CO_O_X", "CO_O_Y")}
   else{coord_name <- c("CO_D_X", "CO_D_Y")}
   
-  return(st_as_sf(database_publico, coords = coord_name, crs = projection))
+  return(st_as_sf(database, coords = coord_name, crs = projection))
 }
   
 
