d579c9981367e15db54b4a4ab94c2297b34940f4
Rodolfo Oliveira Lorenzo <rodolfo@ibureau.com.br>
2020-04-19 12:50:30 -0300

    spatializaing (sp) simulated database

--- 003_spatialize_simulated_data.R
+++ 003_spatialize_simulated_data.R
@@ -1,5 +1,11 @@
 library(dplyr)
+library(sf)
 source('502_simulation_processing_functions.R')
 
-simulated_database_origem <- simulation_data_to_sf(origem = T)
-simulated_database_destino <- simulation_data_to_sf(origem = F)
+simulated_database_origem <- simulation_data_to_sp(origem = T)
+simulated_database_destino <- simulation_data_to_sp(origem = F)
+
+
+simulated_database_origem <- spTransform(simulated_database_origem, CRSobj = CRS(as.character(rgdal::make_EPSG()[rgdal::make_EPSG()$code %in% 31983,]$prj4)))
+simulated_database_destino <- spTransform(simulated_database_destino, CRSobj  = CRS(as.character(rgdal::make_EPSG()[rgdal::make_EPSG()$code %in% 31983,]$prj4)))
+
