c3a1998c1e4154e838810abf4e9d1eaac9d06c18
Rodolfo Oliveira Lorenzo <rodolfo@ibureau.com.br>
2020-04-19 12:51:09 -0300

    using sp package instead of sf

--- 901_convert_spatial.R
+++ 901_convert_spatial.R
@@ -1,13 +1,14 @@
 #funcao para converter as bases da OD para sf
 
-convert_spatial <- function(database, projection, origem = T){
-  require(sf)
+convert_spatial <- function(database, origem = T){
+  require(sp)
   require(dplyr)
   
   if(origem == T){coord_name <- c("CO_O_X", "CO_O_Y")}
-  else{coord_name <- c("CO_D_X", "CO_D_Y")}
-  
-  return(st_as_sf(database, coords = coord_name, crs = projection))
+  if(origem == F){coord_name <- c("CO_D_X", "CO_D_Y")}
+  return(SpatialPointsDataFrame(coords = database[,coord_name], 
+                                data = database, 
+                                proj4string = CRS(as.character(rgdal::make_EPSG()[rgdal::make_EPSG()$code %in% 31983,]$prj4))))
 }
   
 
