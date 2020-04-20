ce972a68fc4a00ba4518b72d119b221b50dabc52
rodolfo-oliveira <ruffus993@gmail.com>
2020-04-20 14:38:48 -0300

    function to associate trips form databases

---
+++ 907_associate_buffer_trips.R
@@ -0,0 +1,26 @@
+#function to associate simulated trips to OD trips
+
+associate_buffer_trips <- function(spatialDatabaseODOrigin,
+                                   spatialDatabaseODestination,
+                                   spatialSimulatedDatabaseOrigin,
+                                   spatialSimulatedDatabaseDestination,
+                                   bufferSize = 1000){
+  source('503_buffering_functions.R')
+  
+  result <- data.frame(IDs = spatialDatabaseODOrigin$DURACAO)
+  result$IDs <- c(NA)
+  for(i in 1:length(spatialDatabaseODOrigin)){
+    aux <- origin_destination_buffer_trip(originSpatialPoint = spatialDatabaseODOrigin[i,],
+                                          simulatedTripsOrigins = spatialSimulatedDatabaseOrigin,
+                                          destinationSpatialPoint = spatialDatabaseODestination[i,],
+                                          simulatedTripsDestinations = spatialSimulatedDatabaseDestination,
+                                          bufferSize = bufferSize)
+    
+    result$IDs[i]  <- list(aux@data$ID)
+    print(paste0(round(100*i/length(spatialDatabaseODOrigin), digits = 2), "%"))
+  }  
+  return(result)
+}
+
+
+
